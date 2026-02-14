94d039d623ea3fc6e410ee91396810484f41659f
Jan Vlnas <jan.vlnas@mews.com>
2026-02-14 23:52:32 +0100

    Simplify interface

--- src/components/operations-interface.tsx
+++ src/components/operations-interface.tsx
@@ -2,14 +2,17 @@ import * as cs from "@alloy-js/csharp";
 import { Threading } from "@alloy-js/csharp/global/System";
 import { Tasks } from "@alloy-js/csharp/global/System/Threading";
 import type { Children } from "@alloy-js/core";
-import { List, refkey, Show } from "@alloy-js/core";
+import { For, refkey } from "@alloy-js/core";
 import type { Program } from "@typespec/compiler";
 import { isVoidType } from "@typespec/compiler";
 import type { HttpOperation, HttpPayloadBody } from "@typespec/http";
 import type { OperationContainer } from "@typespec/http";
 import { useTsp } from "@typespec/emitter-framework";
 import { TypeExpression } from "@typespec/emitter-framework/csharp";
-import type { ActionParameter, NamePolicyLike } from "../utils/operation-to-action.js";
+import type {
+  ActionParameter,
+  NamePolicyLike,
+} from "../utils/operation-to-action.js";
 import {
   getActionMethodInfo,
   getBodyType,
@@ -23,23 +26,26 @@ export interface OperationsInterfaceDeclarationProps {
   namePolicy?: NamePolicyLike;
 }
 
-function buildInterfaceMethodParams(
-  info: { parameters: ActionParameter[] },
-): Array<{ name: string; type: Children; default?: Children }> {
-  const params: Array<{ name: string; type: Children; default?: Children }> = [];
-  for (const ap of info.parameters) {
-    const type =
-      ap.attribute === "FromBody" && "type" in ap.param
-        ? <TypeExpression type={getBodyType(ap.param as unknown as HttpPayloadBody)!} />
-        : <TypeExpression type={getParamType(ap).type} />;
-    params.push({ name: ap.name, type });
-  }
-  params.push({
-    name: "cancellationToken",
-    type: Threading.CancellationToken as Children,
-    default: <>default</>,
-  });
-  return params;
+function buildInterfaceMethodParams(info: {
+  parameters: ActionParameter[];
+}): Array<{ name: string; type: Children; default?: Children }> {
+  return [
+    ...info.parameters.map((ap) => ({
+      name: ap.name,
+      type: (ap.attribute === "FromBody" && "type" in ap.param ? (
+        <TypeExpression
+          type={getBodyType(ap.param as unknown as HttpPayloadBody)!}
+        />
+      ) : (
+        <TypeExpression type={getParamType(ap).type} />
+      )) as Children,
+    })),
+    {
+      name: "cancellationToken",
+      type: Threading.CancellationToken as Children,
+      default: <>default</>,
+    },
+  ];
 }
 
 /**
@@ -52,55 +58,56 @@ export function OperationsInterfaceDeclaration(
   const { program, container, operations } = props;
   const namePolicy = props.namePolicy ?? cs.useCSharpNamePolicy();
   const { $ } = useTsp();
-  const interfaceName = "I" + namePolicy.getName(container.name ?? "Api", "class");
+  const interfaceName =
+    "I" + namePolicy.getName(container.name ?? "Api", "class");
   const containerRoute = undefined; // only needed for action route, not for interface
 
-  const methods: Children[] = [];
-
-  for (const op of operations) {
-    const info = getActionMethodInfo(program, op, containerRoute, namePolicy);
-    const methodName = info.operationName + "Async";
-
-    const paramDescriptors = buildInterfaceMethodParams(info);
-
-    const returnType: Children = isVoidType(op.operation.returnType)
-      ? (Tasks.Task as Children)
-      : (
-        <cs.AccessExpression>
-          <cs.AccessExpression.Part
-            refkey={Tasks.Task}
-            typeArgs={[<TypeExpression type={op.operation.returnType} />]}
-          />
-        </cs.AccessExpression>
-      );
-
-    const operationDoc = $.type.getDoc(op.operation);
-    const doc = operationDoc ? (
-      <cs.DocSummary>
-        <cs.DocFromMarkdown markdown={operationDoc} />
-      </cs.DocSummary>
-    ) : undefined;
-
-    methods.push(
-      <cs.InterfaceMethod
-        public
-        name={methodName}
-        returns={returnType}
-        parameters={paramDescriptors.map((p) => ({
-          name: p.name,
-          type: p.type,
-          default: p.default,
-        }))}
-        doc={doc}
-      />,
-    );
-  }
-
   return (
-    <cs.InterfaceDeclaration public name={interfaceName} refkey={refkey(container)}>
-      <List hardline>
-        {methods}
-      </List>
+    <cs.InterfaceDeclaration
+      public
+      name={interfaceName}
+      refkey={refkey(container)}
+    >
+      <For each={operations} hardline>
+        {(op) => {
+          const info = getActionMethodInfo(
+            program,
+            op,
+            containerRoute,
+            namePolicy,
+          );
+          const methodName = info.operationName + "Async";
+          const paramDescriptors = buildInterfaceMethodParams(info);
+
+          const returnType: Children = isVoidType(op.operation.returnType) ? (
+            (Tasks.Task as Children)
+          ) : (
+            <cs.AccessExpression>
+              <cs.AccessExpression.Part
+                refkey={Tasks.Task}
+                typeArgs={[<TypeExpression type={op.operation.returnType} />]}
+              />
+            </cs.AccessExpression>
+          );
+
+          const operationDoc = $.type.getDoc(op.operation);
+          const doc = operationDoc ? (
+            <cs.DocSummary>
+              <cs.DocFromMarkdown markdown={operationDoc} />
+            </cs.DocSummary>
+          ) : undefined;
+
+          return (
+            <cs.InterfaceMethod
+              public
+              name={methodName}
+              returns={returnType}
+              parameters={paramDescriptors}
+              doc={doc}
+            />
+          );
+        }}
+      </For>
     </cs.InterfaceDeclaration>
   );
 }
